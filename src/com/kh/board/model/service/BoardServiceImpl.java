822173b136f1caed9e494ab966268a9cca757060
DESKTOP-8KD149H\user1 <taegyeong0519@gmail.com>
2026-02-13 17:53:59 +0900

    JDBC

--- src/com/kh/board/model/service/BoardServiceImpl.java
+++ src/com/kh/board/model/service/BoardServiceImpl.java
@@ -1,7 +1,47 @@
 package com.kh.board.model.service;
 
+import java.util.List;
+
+import com.kh.board.model.vo.Board;
+
 // BoardService 인터페이스를 구현하는 클래스.
 // 각 메서드의 설명에 맞게 기능을 작성.
-public class BoardServiceImpl{
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public int login(String memberId, String MemberPwd) {
+
+		return 0;
+	}
+
+	@Override
+	public int insertBoard(Board b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Board> selectBoardList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Board selectBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateBoard(int boardNo, Board b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteBoard(int boardNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
